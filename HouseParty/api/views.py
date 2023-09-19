6372085a99c304454dae6ac105e1dedbea77842d
shahdhesham5 <shahd.hesham1605@gmail.com>
2023-09-19 15:42:32 +0300

    Room model

--- HouseParty/api/views.py
+++ HouseParty/api/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rest_framework import generics
+from .models import Room
+from .serializers import RoomSerializer
 # Create your views here.
 def main(request):
     return HttpResponse("Hello")
+#Using generics create function to allow us create a room instance from the api 
+class RoomView(generics.CreateAPIView):
+    queryset = Room.objects.all()
+    serializer_class = RoomSerializer
+#Using generics List function to only retrieve the data as a list
+class RoomListView(generics.ListAPIView):
+    queryset = Room.objects.all()
+    serializer_class = RoomSerializer
